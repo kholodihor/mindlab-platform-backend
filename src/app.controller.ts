02eeb33b486befb641aace955e448292e9630d7f
cold <kholodihor@gmail.com>
2024-05-07 17:00:04 +0300

    fix: add types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-    getHello(): any {
-        throw new Error('Method not implemented.');
-    }
-    constructor() {}
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
+  constructor() {}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { ForbiddenException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { hashSync } from 'bcrypt';
 import { Role, User } from '@prisma/client';
@@ -6,51 +11,57 @@ import { JwtPayload } from '../auth/interfaces';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async save(user: Partial<User>) {
-        const candidate = await this.findOne(user.email);
-        if (candidate) {
-            throw new HttpException('Користувач з такою поштою вжє інсує', HttpStatus.BAD_REQUEST);
-        }
-
-        const hashedPassword = this.hashPassword(user.password);
-        return this.prismaService.user.create({
-            data: {
-                email: user.email,
-                name: user.name,
-                password: hashedPassword,
-                roles: ['USER'],
-            },
-        });
+  async save(user: Partial<User>) {
+    const candidate = await this.findOne(user.email);
+    if (candidate) {
+      throw new HttpException(
+        'Користувач з такою поштою вжє інсує',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async findOne(idOrEmail) {
-        return this.prismaService.user.findFirst({
-            where: {
-                OR: [
-                    { id: isNaN(Number(idOrEmail)) ? undefined : Number(idOrEmail) },
-                    { email: typeof idOrEmail === 'string' ? idOrEmail : undefined },
-                ],
-            },
-        });
-    }
+    const hashedPassword = this.hashPassword(user.password);
+    return this.prismaService.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashedPassword,
+        roles: ['USER'],
+      },
+    });
+  }
 
-    async delete(id: string, user: JwtPayload) {
-        if (user.id !== id && !user.roles.includes(Role.ADMIN)) {
-            throw new ForbiddenException();
-        }
-        const candidate = await this.findOne(id);
-        if (!candidate) {
-            throw new HttpException('Користувача не знайдено', HttpStatus.BAD_REQUEST);
-        }
-        return this.prismaService.user.delete({
-            where: { id: Number(id) },
-            select: { id: true },
-        });
-    }
+  async findOne(idOrEmail) {
+    return this.prismaService.user.findFirst({
+      where: {
+        OR: [
+          { id: isNaN(Number(idOrEmail)) ? undefined : Number(idOrEmail) },
+          { email: typeof idOrEmail === 'string' ? idOrEmail : undefined },
+        ],
+      },
+    });
+  }
 
-    private hashPassword(password: string) {
-        return hashSync(password, 10);
+  async delete(id: string, user: JwtPayload) {
+    if (user.id !== id && !user.roles.includes(Role.ADMIN)) {
+      throw new ForbiddenException();
     }
+    const candidate = await this.findOne(id);
+    if (!candidate) {
+      throw new HttpException(
+        'Користувача не знайдено',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.prismaService.user.delete({
+      where: { id: Number(id) },
+      select: { id: true },
+    });
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, 10);
+  }
 }
